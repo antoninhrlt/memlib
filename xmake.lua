973b04749b6087f786dd3bd19f6654f9546e6d8d
antoninhrlt <antoninhrlt1@gmail.com>
2022-05-20 23:27:11 +0200

    Add the DEBUG flag when debug mode

--- xmake.lua
+++ xmake.lua
@@ -2,6 +2,11 @@
 -- Under the MIT License
 -- Copyright (c) 2022 Antonin Hérault
 
+add_rules("mode.debug", "mode.release")
+
 target("memlib")
     set_kind("shared")
     add_files("src/*.c")
+    if is_mode("debug") then
+        add_defines("DEBUG")
+    end
